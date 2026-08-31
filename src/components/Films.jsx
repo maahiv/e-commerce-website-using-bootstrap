ee2b819af6bd98f39c3de3200f53f7eea64f5dbb
maahiv <maahiv521@gmail.com>
2026-08-31 20:27:35 +0530

    Add loading state to films API

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,35 +1,43 @@
-import { useEffect, useState } from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { useState } from "react";
+import { Container, Card, Button, Spinner } from "react-bootstrap";
 
 function Films() {
   const [films, setFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchFilms = async () => {
-    const response = await fetch('https://swapi.info/api/films');
+    setIsLoading(true);
 
+    const response = await fetch("https://swapi.info/api/films");
     const data = await response.json();
 
     setFilms(data);
+    setIsLoading(false);
   };
-
-  useEffect(() => {
-    fetchFilms();
-  }, []);
 
   return (
     <Container className="py-5">
       <h1 className="text-center mb-4">Star Wars Films</h1>
 
-      {films.map((film) => (
-        <Card className="mb-3" key={film.episode_id}>
-          <Card.Body>
-            <Card.Title>{film.title}</Card.Title>
-            <Card.Text>
-              Episode: {film.episode_id}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      <div className="text-center mb-4">
+        <Button onClick={fetchFilms}>Get Films</Button>
+      </div>
+
+      {isLoading && (
+        <div className="text-center">
+          <Spinner animation="border" />
+        </div>
+      )}
+
+      {!isLoading &&
+        films.map((film) => (
+          <Card className="mb-3" key={film.episode_id}>
+            <Card.Body>
+              <Card.Title>{film.title}</Card.Title>
+              <Card.Text>Episode: {film.episode_id}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
     </Container>
   );
 }
